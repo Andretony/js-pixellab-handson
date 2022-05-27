a6dc87c1d2cf7d0a367715bc5819fea9d2411b54
Andretony <75176677+Andretony@users.noreply.github.com>
2022-05-27 14:01:40 +0300

    tema

--- M01S03/ex01/app.js
+++ M01S03/ex01/app.js
@@ -2,6 +2,8 @@ var object = {
   name: 'Andrei',
   surname: 'Anton',
   age: 32,
+  petOwner: false,
+  carOwner: true,
 };
 
 console.log(object.name);
@@ -19,3 +21,11 @@ console.log(
 );
 
 console.log((2022 - object.age).toString());
+
+console.log(
+  object.name + ' ' + object.surname + ' are ' + object.age + ' ani' + '.',
+);
+
+console.log(
+  object.name + ' s-a nascut in ' + (2022 - object.age).toString() + '.',
+);
