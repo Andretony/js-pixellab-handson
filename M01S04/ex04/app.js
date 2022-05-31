b30b48fe65c15980d54ecd3be74ec636432931b2
Andretony <75176677+Andretony@users.noreply.github.com>
2022-05-31 21:59:35 +0300

    1000

---
+++ M01S04/ex04/app.js
@@ -0,0 +1,40 @@
+var i = 0;
+
+// for (i = 0; i <= 3; i++) {
+//   console.log(i);
+// }
+
+// Modifica exemplul astefl incat bucla sa numere de la 1 la 100
+// for (i = 1; i <= 100; i++) {
+//   console.log(i);
+// }
+
+// Modifiaca exempul astfel incat bucla sa numere de la 1 la 99(dar nu schimba numarul 100)
+// for (i = 1; i < 100; i++) {
+//   console.log(i);
+// }
+
+// folosind keywordul brak opreste bucla atunci cand numarul este egal cu 50(dar afiseazal)
+// for (i = 1; i < 100; i++) {
+//   console.log(i);
+
+//   if (i === 50) {
+//     break;
+//   }
+// }
+
+// folosind keywordul continue afiseaza doar numerele pare intre 1 si 100(inclusiv 100)
+// for (i = 1; i <= 100; i++) {
+//   if (i % 2 !== 0) {
+//     continue;
+//   }
+
+//   console.log(i);
+// }
+
+// in mod similar, afiseaza doar numerele care sunt divizibile cu 5
+// for (i = 1; i <= 100; i++) {
+//   if (i % 5 === 0) {
+//     console.log(i);
+//   }
+// }
---
+++ M01S04/ex06/app.js
@@ -0,0 +1,33 @@
+var person = {
+  name: 'Andrei',
+  surname: 'Anton',
+  age: 32,
+  petOwner: false,
+  skills: [
+    'html',
+    'javascript',
+    'css',
+    'java',
+    'c++',
+    'node',
+    'jquery',
+    'node.js',
+  ],
+  friends: [
+    {
+      name: 'Larry',
+      surname: 'Larrynson',
+      age: 30,
+    },
+    {
+      name: 'Steven',
+      surname: 'Stevenson',
+      age: 31,
+    },
+    {
+      name: 'Carol',
+      surname: 'Carolson',
+      age: 29,
+    },
+  ],
+};
